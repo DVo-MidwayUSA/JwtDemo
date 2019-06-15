717074babbcb5c4a7b3fb800662e6bca4b7de55e
Daniel Vo <dvo@midwayusa.com>
2019-06-15 07:34:22 -0500

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+const app = express()
+app.use(bodyParser.json())
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+const PORT = 8888;
+
+app.get('/time', (request, response) => {
+    const time = (new Date()).toLocaleTimeString();
+    response.status(200)
+        .send(`The time is ${time}`)
+})
+
+app.get('*', (request, response) => {
+    response.sendStatus(404);
+})
+
+app.listen(PORT, () => {
+    console.log(`Express server is running on port ${PORT}`)
+})
+
+app.post('/login', (request, response) => {
+    console.log(`POST: '/login'`)
+    console.log(request.body)
+    response.status(200).send('OK')
+    // const user = request.body.username
+    // response.status(200).send(`User's name is ${user}`)
+})
