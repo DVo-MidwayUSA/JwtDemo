1f84b6bd9b35b898cc4349a631487d25758cc0e1
Daniel Vo <dvo@midwayusa.com>
2019-06-16 23:25:58 -0500

    working secret verification

--- servers/app-server.js
+++ servers/app-server.js
@@ -20,6 +20,15 @@ app.listen(PORT, () => {
     console.log(`App server is running on port ${PORT}`)
 })
 
+const secrets = [
+    { userid: 1, secret: 'I am Superman'},
+    { userid: 1, secret: 'secret 2'},
+    { userid: 1, secret: 'sec'},
+    { userid: 2, secret: 'I am Batman'},
+    { userid: 2, secret: '2'},
+    { userid: 2, secret: '3'},
+]
+
 app.get('/secrets', (request, response) => {
     const token = request.cookies.access_token
     if (!token) {
@@ -27,12 +36,14 @@ app.get('/secrets', (request, response) => {
         return
     }
 
-    const decoded = jwt.verify(token, 'mykey')
-    console.log(decoded)
-    
-    response.status(200).send({})
+    const user = jwt.verify(token, 'mykey')
+    response.status(200).send(getSecretsByUser(user))
 })
 
 app.get('*', (request, response) => {
     response.sendStatus(404)
 })
+
+const getSecretsByUser = (user) => {
+    return secrets.filter((secret) => secret.userid == user.id)
+}
